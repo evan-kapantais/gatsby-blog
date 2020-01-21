e4e3e3d1cec8ca6bf01469757e7423b2c9c1169b
Evan Kapantais <evankapantais@gmail.com>
2020-01-21 16:59:35 +0100

    almost finalise contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -94,14 +94,17 @@ class ContactForm extends React.Component {
 
   onFocus = () => {
     this.labelRef.current.style.top = '-1.5rem';
+    this.labelRef.current.style.fontSize = '0.8rem';
   }
-
+  
   onBlur = () => {
     if (this.state.name === '') {
       this.labelRef.current.style.top = '0.5rem';
       this.labelRef.current.style.color = 'red';
+      this.labelRef.current.style.fontSize = '1rem';
     } else {
       this.labelRef.current.style.top = '-1.5rem';
+      this.labelRef.current.style.fontSize = '0.8rem';
     }
   }
 
@@ -113,15 +116,15 @@ class ContactForm extends React.Component {
     return (
       <Form name='contact' data-netlify='true'>
         <div>
-          <input type='text' id='name' placeholder='' onChange={this.onChange} value={this.state.name} onFocus={this.onFocus} onBlur={this.onBlur} />
-          <label htmlFor='name' ref={this.labelRef}>Name</label>
+          <input type='text' id='name' placeholder='' onChange={this.onChange} value={this.state.name} onFocus={this.onFocus} onBlur={this.onBlur} required />
+          <label htmlFor='name' ref={this.labelRef}>* Name</label>
         </div>
-        {/* <label htmlFor='email'>Email</label> */}
+        {/* <label htmlFor='email'>* Email</label> */}
         <input type='email' placeholder='Email' />
         {/* <label htmlFor='subject'>Subject</label> */}
-        <input type='text' placeholder='Hey Evan!' />
-        {/* <label htmlFor='message'>Message</label> */}
-        <textarea name='message' id='message' cols='30' rows='5' placeholder='Yout Message Here' />
+        <input type='text' placeholder='Hey Evan!' required />
+        {/* <label htmlFor='message'>* Message</label> */}
+        <textarea name='message' id='message' cols='30' rows='5' placeholder='Yout Message Here' required />
         <Buttons>
           <input type='submit' value='Submit' />
           <input type='reset' value='Reset' />
@@ -131,7 +134,7 @@ class ContactForm extends React.Component {
   }
 }
 
-const Container = styled.div `
+const Container = styled.div`
   max-width: 1200px;
   margin: 4rem auto;
 
@@ -140,20 +143,24 @@ const Container = styled.div `
   }
 `
 
-const Email = styled.section `
+const Email = styled.section`
   display: flex;
 `
 
 const SectionHeading = styled.h1`
   font-size: 6rem;
   text-align: center;
-  margin-bottom: 4rem;
+  margin: 0;
   text-align: left;
-  text-shadow: 5px 5px rgb(3, 159, 255);
   text-shadow: 5px 5px lightskyblue;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    text-shadow: 10px 10px wheat;
+  }
 `
 
-const Mail = styled.section `
+const Mail = styled.section`
   display: flex;
 
   h1 {
@@ -161,13 +168,13 @@ const Mail = styled.section `
   }
 `
 
-const AddressContainer = styled.div `
-    min-width: 600px;
-    display: flex;
-    flex-direction: column;
+const AddressContainer = styled.div`
+  min-width: 600px;
+  display: flex;
+  flex-direction: column;
 `
 
-const Address = styled.div `
+const Address = styled.div`
   display: flex;
   align-items: center;
 
@@ -181,7 +188,7 @@ const Address = styled.div `
   }
 `
 
-const float = keyframes `
+const float = keyframes`
   0% {
     transform: translate(0, 0);
   }
@@ -194,13 +201,57 @@ const float = keyframes `
   }
 `
 
-const Image = styled.img `
-  display: block;
+const Image = styled.img`
   max-width: 160px;
   max-height: 160px;
   margin-right: 4rem;
   animation: ${float} 5s ease-in-out infinite;
   animation-delay: ${props => props.delay || 0};
+`
+
+const wiggle = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  33% {
+    transform: rotate(5deg);
+  }
+  66% {
+    transform: rotate(-5deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`
+
+const SocialImage = styled(Image)`
+  margin: 0;
+`
+
+
+const Social = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+
+  a {
+    display: block;
+    transition: all 300ms ease;
+
+    &:hover {
+      transform: scale(1.1);
+      animation: ${wiggle} 300ms ease-in-out both alternate;
+    }
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(3, 160px);
+    grid-template-rows: 160px 160px;
+    align-items: center;
+    gap: 4rem;
+    min-width: 600px;
+  }
 `
 
 const contactPage = () =>  {
@@ -210,7 +261,7 @@ const contactPage = () =>  {
       <Seo title='Contact' />
       <Container>
         <Email>
-          <SectionHeading>You can reach me through email &rarr;</SectionHeading>
+          <SectionHeading>You can reach me by email, <br /> &rarr;</SectionHeading>
           <ContactForm />
         </Email>
         <Mail>
@@ -228,7 +279,7 @@ const contactPage = () =>  {
            <Address>
              <Image delay='1000ms' src={require('../images/building.png')} alt='company' />
              <p>
-               Carrer de Llacuna <br />
+               Carrer de la Llacuna 166 <br />
                10th Floor <br />
                08018 <br />
                Barcelona <br />
@@ -236,8 +287,28 @@ const contactPage = () =>  {
              </p>
            </Address>
           </AddressContainer>
-          <SectionHeading>or via regular mail. <br /> &larr;</SectionHeading>
+          <SectionHeading>via <br /> regular mail, <br /> &larr;</SectionHeading>
         </Mail>
+        <Social>
+          <SectionHeading>or my <br /> social <br /> media. <br />&rarr;</SectionHeading>
+          <div>
+            <a href='https://github.com/evan-kapantais'>
+              <SocialImage src={require('../images/github-colour.png')} alt='github icon' delay='100ms' />
+            </a>
+            <a href='https://twitter.com/evankapantais'>
+              <SocialImage src={require('../images/twitter-colour.png')} alt='twitter icon' delay='600ms' />
+            </a>
+            <a href='https://www.instagram.com/evan_kapantais/'>
+              <SocialImage src={require('../images/instagram-colour.png')} alt='instagram icon' delay='1100ms' />
+            </a>
+            <a href='facebook.com'>
+              <SocialImage src={require('../images/facebook-colour.png')} alt='facebook icon' delay='600ms' />
+            </a>
+            <a href='https://www.linkedin.com/in/evankapantais/'>
+              <SocialImage src={require('../images/linkedin-colour.png')} alt='linkedin icon' delay='1100ms' />
+            </a>
+          </div>
+        </Social>
       </Container>
     </Layout>
   );
